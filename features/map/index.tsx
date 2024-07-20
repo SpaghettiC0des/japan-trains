64d5f609294e7bc436d338eaef7f02577003c664
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2024-07-21 05:59:56 +0800

    feat: initial map of the train stations

---
+++ features/map/index.tsx
@@ -0,0 +1,42 @@
+import MapView, { Marker } from "react-native-maps";
+import { View } from "tamagui";
+
+import { trains } from "shared/train";
+
+type Props = { prefectureId: string; lineId: string };
+export function TrainMap({ lineId, prefectureId }: Props) {
+  const prefecture = trains.find(
+    (station) => station.id === Number(prefectureId),
+  );
+  const line = prefecture?.lines.find((l) => l.id === Number(lineId));
+
+  if (!line) return null;
+
+  return (
+    <View flex={1}>
+      <MapView
+        region={{
+          latitude: line.lat,
+          longitude: line.lng,
+          longitudeDelta: 0.5,
+          latitudeDelta: 0.5,
+        }}
+        style={{ width: "100%", height: "100%" }}
+      >
+        {line.stations.map((station) => {
+          return (
+            <Marker
+              key={station.id}
+              coordinate={{
+                latitude: station.location.lat,
+                longitude: station.location.lng,
+              }}
+              description={"Postal Code: " + station.location.postalCode.en}
+              title={station.name.en}
+            />
+          );
+        })}
+      </MapView>
+    </View>
+  );
+}
